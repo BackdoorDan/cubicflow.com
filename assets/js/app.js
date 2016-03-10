6f86f390451becd276febfc6c72282ee21a4d831
BackdoorDan <d.anmcn.air@gmail.com>
2016-03-10 16:48:14 +0900

    pair case stucy page

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,29 +5,35 @@
 //
 // Initialize Foundation
 // --------------------------------------------------
-
 $(document).foundation();
 
 
 //
 // Custom JS
 // --------------------------------------------------
+$(document).on('ready', function(){
 
-cf.assets.init();
+  // Init asset replacement
+  cf.assets.init();
 
-cf.modalGallery('.shapshots-grid__snapshot');
+  // If snapshots exist init modalGallery
+  if (document.querySelectorAll('.shapshots-grid__snapshot').length > 1) {
+      cf.modalGallery('.shapshots-grid__snapshot');
+  }
 
+  // Mobile Nav
+  var $expandBtn = $('.nav-toggle-btn');
+  var $mobileNav = $('.nav__expanded');
+  var $mobileNavOverlay = $('.nav__expanded-overlay');
 
-//
-var $expandBtn = $('.nav-toggle-btn');
-var $mobileNav = $('.nav__expanded');
-var $mobileNavOverlay = $('.nav__expanded-overlay');
+  var toggleNav = function(){
+    $mobileNav.toggleClass('active');
+    $mobileNavOverlay.toggleClass('active');
+    $expandBtn.toggleClass('active');
+  };
 
-var toggleNav = function(){
-  $mobileNav.toggleClass('active');
-  $mobileNavOverlay.toggleClass('active');
-  $expandBtn.toggleClass('active');
-};
+  $expandBtn.on('click', toggleNav);
+  $mobileNavOverlay.on('click', toggleNav);
+  //
 
-$expandBtn.on('click', toggleNav);
-$mobileNavOverlay.on('click', toggleNav);
+});
